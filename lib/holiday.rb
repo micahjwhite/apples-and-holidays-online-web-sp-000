5c30259ac2e4d1cb774aadcca5563a7753e15845
Micah White <micahjwhite@gmail.com>
2020-04-15 19:02:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
-    holiday.each do |supplies|
+    holiday.each do |holiday, supplies|
 end
 
 def all_holidays_with_bbq(holiday_hash)
