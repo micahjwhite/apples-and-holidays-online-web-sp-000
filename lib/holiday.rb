ffd5723ffc34909062b3b1cd4ad6e085ef0ca34a
Micah White <micahjwhite@gmail.com>
2020-04-15 18:43:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,8 +27,9 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash.each do |season, holidays_and_supplies|
     holidays_and_supplies.each do |holiday, supply|
+      if holiday == :winter
         
-      end
+      
     end
   end
 end
