38a7a05f5e4e7ce0892e7c05abbbd0f1bb16200f
Micah White <micahjwhite@gmail.com>
2020-04-15 19:13:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     season.each do |holiday, supply|
       if supply.include?("BBQ")
-        return holiday
+        return holiday.flatten
 end
 
 
